202b4148fcb59a0275c2b7e1c6dfc1aa4411229d
Ulysses Popple <ulysses.popple@gmail.com>
2019-06-23 02:06:50 -0400

    Nearby

---
+++ Assets/Scripts/MlAgents/ScriptableObjects/MLRewardNearby.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using MLAgents;
+
+[CreateAssetMenu(menuName="ML/Rewards/Nearby")]
+class MLRewardNearby : MLReward {
+    public string Tag;
+    public float Reward;
+    public float Distance;
+
+    public override void AddReward(BaseAgent agent, float[] vectorActions) {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag(Tag);
+        foreach(GameObject obj in objs) {
+            if ((obj.transform.position - agent.gameObject.transform.position).sqrMagnitude < Distance) {
+                agent.AddReward(Reward);
+            }
+        }
+    }
+}
