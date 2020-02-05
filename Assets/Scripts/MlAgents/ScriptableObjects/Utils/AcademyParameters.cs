5dc495f30c8c4252710ca3f299179ffecde48ab5
Ulysses Popple <ulysses.popple@gmail.com>
2020-02-05 13:59:58 -0500

    Most recent mlagents

--- Assets/Scripts/MlAgents/ScriptableObjects/Utils/AcademyParameters.cs
+++ Assets/Scripts/MlAgents/ScriptableObjects/Utils/AcademyParameters.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 static class AcademyParameters {
 
     public static float FetchOrParse(Academy academy, string key) {
-        if(academy.resetParameters.ContainsKey(key)) {
-            return academy.resetParameters[key];
-        } else {
-            return float.Parse(key);
-        }
+      float prop = academy.FloatProperties.GetPropertyWithDefault(key, -1);
+      if(prop == -1) {
+        float.TryParse(key, out prop);
+      }
+
+      return prop;
     }
 
     public static float Update(Academy academy, string key, float current) {
-        if(academy.resetParameters.ContainsKey(key)) {
-            return academy.resetParameters[key];
-        } else {
-            return current;
-        }
+      return academy.FloatProperties.GetPropertyWithDefault(key, current);
     }
 }
